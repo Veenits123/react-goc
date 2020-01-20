364c58325d059ca3059d6e31e1d968c7c48511e3
varunvishal10 <varunvishal10@gmail.com>
2020-01-20 11:28:27 +0530

    Updates

---
+++ precache-manifest.5b174dcd7e94d088e1146bfb2eec942f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a927d6b010a16e8e8520eff065543678",
+    "url": "/react-goc/index.html"
+  },
+  {
+    "revision": "d283343b8d35caeecc3b",
+    "url": "/react-goc/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "ebee267ece77ef3d5a92",
+    "url": "/react-goc/static/css/main.933cad75.chunk.css"
+  },
+  {
+    "revision": "d283343b8d35caeecc3b",
+    "url": "/react-goc/static/js/2.142f4bb3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react-goc/static/js/2.142f4bb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebee267ece77ef3d5a92",
+    "url": "/react-goc/static/js/main.10481eed.chunk.js"
+  },
+  {
+    "revision": "506924c5c39e7c539ffb",
+    "url": "/react-goc/static/js/runtime-main.9ca056fc.js"
+  },
+  {
+    "revision": "fe9995e35f4f13696a471dd6729ab032",
+    "url": "/react-goc/static/media/accounting.fe9995e3.png"
+  },
+  {
+    "revision": "86299ca88aebfe6d82fda2ed3117be39",
+    "url": "/react-goc/static/media/hel.86299ca8.jpg"
+  },
+  {
+    "revision": "55b08045a8e7feeac24cfd8aeef895bf",
+    "url": "/react-goc/static/media/pg.55b08045.png"
+  },
+  {
+    "revision": "dcab4200d70d966efe0aa1f6ebcf746f",
+    "url": "/react-goc/static/media/picture.dcab4200.jpg"
+  },
+  {
+    "revision": "630d92b7b28d8f1d46cc8514c699ca9d",
+    "url": "/react-goc/static/media/safety.630d92b7.jpeg"
+  }
+]);
